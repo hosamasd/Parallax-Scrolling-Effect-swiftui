934b32cf24f70f034e402fc98df40c43564a74b4
hosam <hosamelmalt@gmail.com>
2020-12-16 07:48:17 +0200

    Initial Commit

---
+++ Parallax Scrolling Effect/Parallax_Scrolling_EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Parallax_Scrolling_EffectApp.swift
+//  Parallax Scrolling Effect
+//
+//  Created by hosam on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct Parallax_Scrolling_EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
